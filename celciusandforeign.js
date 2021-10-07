fba9635ce903121895b374f208c582e688f1dff9
zishan@0509 <zishanali20071@gmail.com>
2021-10-07 17:20:25 +0530

    starting of js

--- celciusandforeign.js
+++ celciusandforeign.js
@@ -1,1 +1,16 @@
-let a =9
+let inp = document.querySelector("input")
+let first = document.querySelector(".second__first1")
+let first1 = document.querySelector(".second__first2")
+
+
+function color1(e){
+    e.preventDefault()
+if(first.style.backgroundColor===""){
+    first.style.backgroundColor="blue"
+console.log(first.style)
+}}
+
+// function name1(){
+//     console.log("hfh")
+// }
+first1.onclick =color1
