0f92c685faed256cedc55cbc7d9555ebfd07e6f7
zishan@0509 <zishanali20071@gmail.com>
2021-10-08 15:09:47 +0530

    choose celcius and fahrenhiet

--- celciusandforeign.js
+++ celciusandforeign.js
@@ -3,6 +3,7 @@ let first = document.querySelector(".second__first1")
 let first1 = document.querySelector(".second__first2")
 let second= document.querySelector(".second__second1")
 let second1 = document.querySelector(".second__second2")
+let butt = document.querySelector("#but")
 
 
 
@@ -28,7 +29,20 @@ function color1(e){
         
     }}
     }
+
+    function cli(){
+        if(first.style.backgroundColor==="blue"){
+            window.alert("celsius")
+        }else if(second.style.backgroundColor==="blue"){
+            window.alert("fahrenhiet")
+        }
+    }
+//    function cli(e){
+//        e.preventDefault()
+//        window.alert("he")
+//    }
   
 
 first1.onclick=color
 second1.onclick=color1;
+butt.onclick=cli
